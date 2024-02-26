b99b5b57032c308c4c39cef1497a80c4186982dd
VAQYA <34599865+VAQYA@users.noreply.github.com>
2024-02-26 14:00:19 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          布袋猫的ChatGPT
+          布袋猫的ChatGPT 
         </div>
         <div className={styles["sidebar-sub-title"]}>
           需要ChatGPT或GPT Plus账号添加微信：FYXXLYY
